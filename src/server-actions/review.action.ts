afec61b77db800cf973e6e68d445ec4d28d1c230
dattv23 <datvan635@gmail.com>
2024-11-25 12:35:07 +0700

    fix: field name review

--- src/server-actions/review.action.ts
+++ src/server-actions/review.action.ts
@@ -21,7 +21,7 @@ const reviewAction = async (
         Accept: 'application/json',
         Authorization: `Bearer ${token?.value}`
       },
-      body: JSON.stringify({ ...formData, productId })
+      body: JSON.stringify({ ...formData, bookId: productId })
     })
 
     const result = (await res.json()) as ApiResponse<Review>
